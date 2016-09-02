a0c067baeeafe6856637c6c9240372e8867429b4
ksasagawa <SasagawaK3540@sduhsd.lan>
2016-09-02 13:14:13 -0700

    Lesson 01

---
+++ APlesson_01/ASCII_2.java
@@ -0,0 +1,17 @@
+public class ASCII_2
+{
+	public static void main(String[]args)
+	{
+	System.out.println(" _______________________");
+	System.out.println(" [____________________]  \\");
+	System.out.println(" [____________________] __\\");
+	System.out.println(" [____________________]|__|\\");
+	System.out.println(" [____________________]         |");
+	System.out.println(" [____________________]         |");
+	System.out.println("_[____________________]_____|");
+	System.out.println(" --___---------------___");
+	System.out.println(" /           \\         /           \\");
+	System.out.println("(     O      )       (     O     )");
+	System.out.println(" \\_____/           \\_____/");
+	}
+}
