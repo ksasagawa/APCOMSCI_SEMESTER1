83e9ca1d93a7544c5d15c6a4b9474e5c4975e4c3
ksasagawa <SasagawaK3540@sduhsd.lan>
2016-09-07 13:32:15 -0700

    Added Lab_02

---
+++ APlesson_02/Lab_02.java
@@ -0,0 +1,24 @@
+public class Lab_02	
+{
+	public static void main(String[]args)
+	{
+		int num1 = 34;
+		int num2 = 15;
+		int multSolv = num1 * num2;
+		System.out.println(num1 + " Muliplied by" + " " + num2 + "\nEquals " + multSolv + "\n");
+		
+		String Name = "Kyle Sasagawa";
+		String Address = "2564 Glenn St.";
+		String City = "Encinitas";
+		String zipCode = "92027";
+		
+		System.out.println(Name + "\n" + Address + "\n" + City + "\n" + zipCode);
+		
+		int L = 15;
+		int H = 30;
+		int W = 2;
+		double sfArea = 2 * (L * H + L * W + W * H);
+		
+		System.out.println("\nThe surface area of your rectangle is " + sfArea);
+	}
+}
