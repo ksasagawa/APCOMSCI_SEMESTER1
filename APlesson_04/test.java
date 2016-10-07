bff94c4f0a630f69acf143cd7f5e85fdf519e5d1
ksasagawa <kyleakira@gmail.com>
2016-10-07 13:41:28 -0700

    Added more to the test.java

--- APlesson_04/test.java
+++ APlesson_04/test.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
 public class test
 {
 	public static void main(String[]args)
 	{
 		double one = 1;
 		double two = 2;
-		testNum(one, two);
+	//	testNum(one, two);
 	//	System.out.printf("This is a test %10.2f %10f", testNum(), (one+two));
+		imps();
 	}
 	public static void testNum(double one, double two){
 		System.out.printf("This is a test %10.2f %10f %10f", one, two, testRet(one, two));
 	}
 	public static double testRet(double one, double two){
 		return one + two;
+	}
+	public static void imps(){
+		Scanner k = new Scanner(System.in);
+		System.out.println("A");
+		String a = k.nextLine();
+		System.out.println("b");
+		String b = k.nextLine();
+		System.out.println("c");
+		String c = k.nextLine();
+		System.out.println("d");
+		String d = k.nextLine();
+		System.out.println(a+b+c+d);
 	}
 }
