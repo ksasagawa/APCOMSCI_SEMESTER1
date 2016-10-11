036258eb084ed1ed62b671220b7901b452103836
ksasagawa <kyleakira@gmail.com>
2016-10-11 14:13:12 -0700

    Updated Reciept for lesson 5

--- APlesson_05/Reciept.java
+++ APlesson_05/Reciept.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class Reciept
 {
-	static double total;
 	public static void main(String[]args)
 	{
 		Reciept r = new Reciept();
@@ -22,29 +21,26 @@ public class Reciept
 		String i4 = k.next();
 		System.out.println("What is the price of the fourth item?");
 		double p4 = k.nextDouble();
-		total = p1+p2+p3+p4;
-		discount();
+		double total = p1+p2+p3+p4;
+		double discount = discount(total);
+		double tax = total*.08;
 		System.out.println("<<<<<<<<<__Receipt__>>>>>>>>>>>>>>");
 		r.print(i1, p1);
 		r.print(i2, p2);
 		r.print(i3, p3);
 		r.print(i4, p4);
-		System.out.printf("\n*%10s______________%8.2f", "Total",total);
-		if (total >= 2000)
-		{
-		System.out.printf("\n*%10s______________15", "Discount");
-		}
-		else
-		{
-		System.out.printf("\n*%10s______________0", "Discount");	
-		}
+		System.out.printf("\n*%10s______________%8.2f", "Subtotal",total);
+		System.out.printf("\n*%10s______________%8.2f", "Discount", discount);
+		System.out.printf("\n*%10s______________%8.2f", "Tax", tax);
+		System.out.printf("\n*%10s______________%8.2f", "Total", total-discount+tax);
+		System.out.println("\n_________________________________________________________\n\t\t\t Thank You!");
 	}
-	public static void discount()
+	public static double discount(double total)
 	{
 		if (total >= 2000)
-		{
-			total = total-(total*.15);
-		}
+			return (total*.15);
+		else
+			return 0.0;
 	}
 	public void print(String entry, double price)
 	{
