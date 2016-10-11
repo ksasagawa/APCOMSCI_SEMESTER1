ee81ae31a3e7ded1fbbe73ffc8353d67c618df8f
ksasagawa <kyleakira@gmail.com>
2016-10-11 14:25:56 -0700

    Updated test file

--- APlesson_04/test.java
+++ APlesson_04/test.java
@@ -5,12 +5,12 @@ public class test
 	{
 		double one = 1;
 		double two = 2;
-	//	testNum(one, two);
+		testNum(one, two);
 	//	System.out.printf("This is a test %10.2f %10f", testNum(), (one+two));
 		imps();
 	}
 	public static void testNum(double one, double two){
-		System.out.printf("This is a test %10.2f %10f %10f", one, two, testRet(one, two));
+		System.out.printf("This is a test %10.2f %10f %10f %10f", one, two, testRet(one, two), one-two);
 	}
 	public static double testRet(double one, double two){
 		return one + two;
