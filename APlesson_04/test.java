83f8b0d23d8630323a54f43413219155c9af4b4a
ksasagawa <kyleakira@gmail.com>
2016-11-02 14:02:01 -0700

    test

--- APlesson_04/test.java
+++ APlesson_04/test.java
@@ -3,7 +3,13 @@ public class test
 {
 	public static void main(String[]args)
 	{
-		double one = 1;
+		
+		/*if (!(0>0)&&!(0*5 < 100)) 
+			System.out.println("AAA");
+		else
+			System.out.println("BBB");
+		*/
+		/*double one = 1;
 		double two = 2;
 		testNum(one, two);
 		//System.out.printf("This is a test %10.2f %10f", testNum(), (one+two));
@@ -12,7 +18,8 @@ public class test
 	public static void testNum(double one, double two){
 		System.out.printf("This is a test %10.2f %10f %10f %.2f\n", one, two, testRet(one, two), one-two);
 	}
-	public static double testRet(double one, double two){
+	public static double testRet(double one, double two)
+	{
 		return one + two;
 	}
 	public static void imps(){
@@ -25,6 +32,6 @@ public class test
 		String c = k.nextLine();
 		System.out.println("d");
 		String d = k.nextLine();
-		System.out.println(a+b+c+d);
+		System.out.println(a+b+c+d);*/
 	}
 }
