72f2a81ea0042430d047c63b420d7983389ae924
ksasagawa <kyleakira@gmail.com>
2016-11-30 13:20:18 -0800

    Finished primes

--- APlesson_10/Prime.java
+++ APlesson_10/Prime.java
@@ -6,16 +6,21 @@ public class Prime
 	{
 		Integer[] numbers = {2,6,8,9,10,12,13,15,17,24,55,66,78,77,79};
 		ArrayList<Integer> numList = new ArrayList<>(Arrays.asList(numbers));
-		if(gFactor(numList,i) == 0)
-			numList.remove(i);
+		for(int i = 0; i<numList.size(); i++)
+		{
+			numList.set(i,gFactor(numList,i));
+			if(gFactor(numList,i) == 0)
+				numList.remove(i);
+		}
 		System.out.println(numList);
 	}
-	public static int gFactor(ArrayList<Integer> n)
+	public static int gFactor(ArrayList<Integer> n, int i)
 	{
-		for (int a = 2; i < n.get(i);a++)
-		if (n.get(i)%a == 0)
-			return n.get(i);
-		else
-			return 0;
+		for(int a =2; a<n.get(i); a++)
+		{
+			if (n.get(i)%a == 0)
+				return n.get(i);
+		}
+		return 0;
 	}
 }
