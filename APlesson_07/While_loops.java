328974a8a093cd29023f7bde8a808a4cc16d2ae6
ksasagawa <kyleakira@gmail.com>
2016-12-02 14:00:47 -0800

    fixed lesson 07

--- APlesson_07/While_loops.java
+++ APlesson_07/While_loops.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class While_loops
 {
 	static int number;
-	static int digits;
+	static int digits =0;
 	public static void main(String[]args)
 	{
 		Scanner k = new Scanner(System.in);
-		System.out.println("Please enter a number:");
+		System.out.println("Please enter a number: ");
 		number = k.nextInt();
 		countDigits();
 		System.out.println(number + " has " + digits + " digits.");
@@ -14,7 +14,7 @@ public class While_loops
 	public static void countDigits()
 	{
 		int num = number;
-		while (number > 0)
+		while (num > 0)
 		{
 			digits += 1;
 			num /= 10;
