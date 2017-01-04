f4f9255c17282e187e54bb43868cf0da116192f7
ksasagawa <kyleakira@gmail.com>
2017-01-04 14:11:29 -0800

    a

--- APlesson_10/Prime.java
+++ APlesson_10/Prime.java
@@ -4,23 +4,33 @@ public class Prime
 {
 	public static void main(String[]args)
 	{
+		int a = 2;
 		Integer[] numbers = {2,6,8,9,10,12,13,15,17,24,55,66,78,77,79};
 		ArrayList<Integer> numList = new ArrayList<>(Arrays.asList(numbers));
 		for(int i = 0; i<numList.size(); i++)
 		{
-			numList.set(i,gFactor(numList,i));
-			if(gFactor(numList,i) == 0)
+			numList.set(i,gFactor(numList,i,a));
+			if(numList.get(i) == 0)
 				numList.remove(i);
 		}
 		System.out.println(numList);
 	}
-	public static int gFactor(ArrayList<Integer> n, int i)
+	public static int gFactor(ArrayList<Integer> n, int i, int a)
 	{
-		for(int a = 2; a<n.get(i); a++)
+		while(a<n.get(i))
 		{
-			if (n.get(i)%a == 0 && a != n.get(i))
-				return n.get(i);
+			if(a != n.get(i))
+			{
+				if (n.get(i)%a == 0)
+				{
+					a = 2;
+					return 0;
+				}
+			a++;
+			gFactor(n,i,a);
+			}
 		}
-		return 0;
+		a =2;
+		return n.get(i);
 	}
 }
