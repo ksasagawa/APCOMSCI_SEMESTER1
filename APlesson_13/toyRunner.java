272dc28ebdca4bea73c0c6f0f4ae4bc87d34d616
ksasagawa <kyleakira@gmail.com>
2017-02-03 14:19:30 -0800

    oof

--- APlesson_13/toyRunner.java
+++ APlesson_13/toyRunner.java
@@ -2,9 +2,11 @@ public class toyRunner
 {
 	public static void main(String[]args)
 	{
-		car a = new car("aaaaaass");
+		/*car a = new car("aaaaaass");
 		aFigure b =new aFigure("bbbbbbbbbbbbdd");
 		System.out.println(a);
-		System.out.println(b);
+		System.out.println(b);*/
+		toyStore nw = new toyStore("Hotwheel, Car, G.I.Joe, AF, PennyRacer, Car, Matchbox, Car, Star Wars, AF, Pullback, Car, Star Wars, AF");
+		System.out.println(nw);
 	}
 }
--- APlesson_13/toyStore.java
+++ APlesson_13/toyStore.java
@@ -3,27 +3,80 @@ import java.util.Arrays;
 public class toyStore
 {
 	public String list;
-	public static void main(String[]args)
-	{
-		ArrayList<toy> toyList = new ArrayList<toy>();
-		
-	}
+	static public ArrayList<toy> toyList = new ArrayList<toy>();
 	public toyStore()
 	{
 		list = "";
 	}
 	public toyStore(String list)
 	{
-		list = loadToys();
+		loadToys(list);
 	}
-	public static String loadToys(String ts)
+	public static void loadToys(String ts)
 	{
 		ArrayList<String> toys = new ArrayList<String>(Arrays.asList(ts.split(", ")));
-		for (int i = 0; i < toys.size(); i++)
+		for (int i = 0; i < toys.size(); i+=2)
 		{
 			String name = toys.get(i);
 			String type = toys.get(i+1);
-			toys.set(getThatToy(),Toy ty = new Toy(name))
+			toy object = getThatToy(name);
+			if(getThatToy(name) == null)
+			{
+				if (type.equals("Car"))
+					toyList.add(new car(name));
+				else
+					toyList.add(new aFigure(name));
+			}
+			else
+				object.setCount(object.getCount() + 1);
 		}
+	}
+	public static toy getThatToy(String nm)
+	{
+		for(toy object : toyList)
+		{
+			if(object.getName().equals(nm))
+				return object;
+		}
+		return null;
+	}
+	public static String getMostFrequentToy()
+	{
+		String name = "";
+		int max = Integer.MIN_VALUE;
+		for(toy object : toyList)
+		{
+			if(max < object.getCount())
+			{
+				max  = object.getCount();
+				name = object.getName();
+			}
+		}
+		return name;
+	}
+	public static String getMostFrequentType()
+	{
+		int cars = 0;
+		int figures = 0;
+		for(toy object : toyList)
+		{
+			if(object.getType().equals("Car"))
+				cars++;
+			if(object.getType().equals("Action Figure"))
+				figures++;
+		}
+		if(cars<figures)
+		{
+			if(cars == figures)
+				return "Equal Amounts of Cars and Figures";
+			else
+				return "Figures";
+		}
+		else
+			return "Cars";
+	}
+	public String toString()
+	{
+		return ""+toyList;
 	}
 }
