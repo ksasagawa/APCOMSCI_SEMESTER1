2e9912f8d7606596f9b880e9f489c231c72927ba
ksasagawa <kyleakira@gmail.com>
2017-05-05 14:11:28 -0700

    moving things around- final commit before project start

--- Test/test.java
+++ Test/test.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
 public class test
 {
-	public void doIt()
+	public int[][] test_2d_arr_thing()
+	{
+		int[][] a = new int[5][5];
+		for(int x = 0;x<5;x++)
+		{
+			for(int y = 0;y<5;y++)
+				a[x][y] = (int)(Math.random()*100);
+		}
+		return a;
+	}
+	public static void main()
+	{
+		for(int i = 0; i<5; i++)
+			
+	}
+	/*public void doIt()
 	{
 		a();
 		b();
 		c();
+	}
+	public int[][] test_2d_arr_thing()
+	{
+		return new int[5][5];
 	}
 	public void a()
 	{
@@ -26,7 +45,7 @@ public class test
 		testINT b= new testc2();
 		b.doIt();
 		testINT c= new testc3();
-		c.doIt();
+		c.doIt();*/
 		/*int[] arr1 = {0, 6, 0, 4, 0, 0, 2};
 		int count = 0;
 	for (int i = 0; i < arr1.length; i++)
@@ -51,8 +70,8 @@ public class test
 		double two = 2;
 		testNum(one, two);
 		//System.out.printf("This is a test %10.2f %10f", testNum(), (one+two));
-		imps();*/
-	}
+		imps();
+	}*/
 	/*public static void testNum(double one, double two){
 		System.out.printf("This is a test %10.2f %10f %10f %.2f\n", one, two, testRet(one, two), one-two);
 	}
